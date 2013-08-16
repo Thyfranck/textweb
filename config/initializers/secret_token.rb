5989e8d1164afd2f4d560ca0fc6374b54e831f7b
Maruf Hasan Bulbul <maruf.freelancer@gmail.com>
2013-08-16 13:48:28 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Textweb::Application.config.secret_token = '8a9b9e25aa2c9fa10ab396601dc50decc7223524fd52bb981c44ce6a1bd7f9d2a4d8ffdf06e184c7fc5ee258f59ca986e6f01ee1efbd5576e7fd11210d5f0ae4'
