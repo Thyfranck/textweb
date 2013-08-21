b24f0f0e30b405a5a8f2b2d8b548d078c8493b7f
Maruf Hasan Bulbul <maruf.freelancer@gmail.com>
2013-08-22 03:46:48 +0600

    Fixed production host url for email

---
+++ app/admin/users.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register User do
+  config.per_page = 50
+
+  actions :all, :except => [:new, :edit]
+
+  filter :email
+  
+  index do
+    selectable_column
+    column :id
+    column :email
+    column :activation_state
+    column :created_at
+    column "Manage" do |user|
+      link_to "Login", login_admin_user_path(user), :target => "_blank"
+    end
+    default_actions
+  end
+
+  member_action :login do
+    @user = User.find(params[:id])
+    auto_login @user
+    redirect_to @user
+  end
+end
