f732e0bbd8b392d1d916de47fe83dfc281d876fe
Iconsbd <iconsbd@gmail.com>
2013-08-26 19:12:27 +0600

    latest updates

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,9 +4,13 @@ class Vote < ActiveRecord::Base
   belongs_to :link
   belongs_to :user
   
-  after_save :update_link_vote
+  after_save :update_link_vote, :check_user
 
   def update_link_vote
     self.link.update_attribute(:vote, self.link.votes.where(:up => true).count - self.link.votes.where(:down => true).count)
+  end
+
+  def check_user
+    return false if self.user == self.link.user
   end
 end
